64e35e0c4b699b49b2b3efa7f463f476ea455156
Igor Siamykin <sparkisisss@gmail.com>
2023-07-13 18:33:07 +0300

    commit_fin

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,9 @@ let model = {
   numShips: 3,
   shipLength: 3,
   shipsSunk: 0,
-  ships: [{ locations: ['06', '16', '26'], hits: ['', '', '']},
-          { locations: ['24', '34', '44'], hits: ['', '', '']},
-          { locations: ['10', '11', '12'], hits: ['', '', '']},],
+  ships: [{ locations: [0, 0, 0], hits: ['', '', '']},
+          { locations: [0, 0, 0], hits: ['', '', '']},
+          { locations: [0, 0, 0], hits: ['', '', '']}],
   fire: function(guess) {
     for(let i = 0; i < this.numShips; i++) {
       // console.log
@@ -35,6 +35,44 @@ let model = {
     }
     return true;
   },
+
+  generateShipLocations: function() {
+    let locations;
+    for(let i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      }while(this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+  generateShip: function() {
+    let direction = Math.floor(Math.random() * 2);
+    let row, col;
+    let newShipLocations = [];
+      for(let i = 0; i < this.shipLength; i++) {
+        if(direction === 1) {
+          row = Math.floor(Math.random() * this.boardSize);
+          col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+          newShipLocations.push(row + '' + (col+1));
+      }else {
+        row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        col = Math.floor(Math.random() * this.boardSize);
+        newShipLocations.push((row+1) + '' + col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function(locations) {
+    for(let i = 0; i < this.numShips; i++) {
+      let ship = model.ships[i];
+      for(let j =0; j < locations.length; j++) {
+        if(ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
 };
 
 let view = {
@@ -52,14 +90,76 @@ let view = {
   },
 };
 
-model.fire('53');
-model.fire('06');
-model.fire('16');
-model.fire('26');
-model.fire('34');
-model.fire('24');
-model.fire('44');
-model.fire('12');
-model.fire('11');
-model.fire('10');
-model.fire('66');
+let controller = {
+  guesses: 0,
+  processGuess: function(guess) {
+    let location = parseGuess(guess);
+    if(location) {
+      this.guesses++;
+      let hit = model.fire(location);
+      if(hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all battleship, in " + this.guesses + " guesses");
+      }
+    }
+  }
+};
+
+function parseGuess(guess) {
+  let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  if(guess === null || guess.length !== 2) {
+    alert("Error data!");
+  }else {
+    firstChar = guess.charAt(0);
+    let row = abc.indexOf(firstChar);
+    let column = guess.charAt(1);
+    if(isNaN(row) || isNaN(column)) {
+      alert("Opps, is not right");
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert('Opps, is not right!');
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+function init() {
+  let fireButton = document.getElementById('fireButton');
+  fireButton.onclick = handleFireButton;
+  let guessInput = document.getElementById('guessInput');
+  guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
+}
+
+function handleFireButton() {
+  let guessInput = document.getElementById('guessInput');
+  let guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = '';
+}
+
+function handleKeyPress(e) {
+  let fireButton = document.getElementById("fireButton");
+  if(e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+
+window.onload = init;
+
+// controller.processGuess('A0');
+
+// controller.processGuess('A6');
+// controller.processGuess('B6');
+// controller.processGuess('C6');
+
+// controller.processGuess('C4');
+// controller.processGuess('D4');
+// controller.processGuess('E4');
+
+// controller.processGuess('B0');
+// controller.processGuess('B1');
+// controller.processGuess('B2');
